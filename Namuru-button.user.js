402abbf8e41f3851d83193ce2529fa58135dcbc6
yuzu_1203 <git-yuzu@svk.jp>
2017-08-30 11:31:43 +0900

    preloadでautoにするよう変更

--- Namuru-button.user.js
+++ Namuru-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Namuru-button
 // @namespace    https://github.com/yuzulabo
-// @version      1.0
+// @version      1.0.1
 // @description  南無りまくれぇ！
 // @author       neziri_wasabi (yuzu_1203)
 // @match        https://knzkoniisan.m.to/*
@@ -22,6 +22,7 @@
     audio_elem.src = "https://yuzu.tk/files/yuzu_1203/gooooon.wav"; //どこか直リンできそうなとこないかなぁ...
     audio_elem.setAttribute('controls', '0');
     audio_elem.setAttribute('id', 'namuru_audio');
+    audio_elem.setAttribute('preload', 'auto');
 
     var heads = document.getElementsByTagName("head");
     if (heads.length > 0) {
@@ -31,8 +32,8 @@
         document.documentElement.appendChild(node);
         document.documentElement.appendChild(audio_elem);
     }
-    var audio_c = document.getElementById("namuru_audio");
 
+    var audio_c = document.getElementById("namuru_audio");
     $(document).on('click', 'button.star-icon', function() {
         console.log("南無！");
         audio_c.currentTime = 0;
