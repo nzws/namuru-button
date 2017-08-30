1df7e60d9a55332a0cd66b20422befb81b2bb04f
yuzu_1203 <git-yuzu@svk.jp>
2017-08-30 16:50:14 +0900

    Ω＼ζ°)ﾁｰﾝ音を追加

--- Namuru-button.user.js
+++ Namuru-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Namuru-button
 // @namespace    https://github.com/yuzulabo
-// @version      1.1.0
+// @version      1.2.0
 // @description  南無りまくれぇ！
 // @author       neziri_wasabi (yuzu_1203)
 // @match        https://knzkoniisan.m.to/*
@@ -11,7 +11,10 @@
 
 /*
  「ゴーン」効果音について
- 効果音はフリー素材を使用しています。魔王魂: http://maoudamashii.jokersounds.com/
+フリー素材を使用しています。魔王魂: http://maoudamashii.jokersounds.com/
+
+「チーン」効果音について
+フリー素材を使用しています。http://nico.ms/nc42872
 
  レア音声：神崎おにいさんについて
  可視光線様のKNZK_ZONEの一部を使用しています。: http://nico.ms/sm31146004
@@ -37,22 +40,32 @@
     audio2_elem.setAttribute('id', 'namuru2_audio');
     audio2_elem.setAttribute('preload', 'auto');
 
+    var audio3_elem = document.createElement("audio");
+    audio3_elem.src = "https://yuzu.tk/files/yuzu_1203/nc42872.mp3";
+    audio3_elem.setAttribute('controls', '0');
+    audio3_elem.setAttribute('id', 'namuru3_audio');
+    audio3_elem.setAttribute('preload', 'auto');
+
     var heads = document.getElementsByTagName("head");
     heads[0].appendChild(node);
     heads[0].appendChild(audio_elem);
     heads[0].appendChild(audio2_elem);
+    heads[0].appendChild(audio3_elem);
 
     var audio_d = document.getElementById("namuru_audio");
     var audio_d2 = document.getElementById("namuru2_audio");
+    var audio_d3 = document.getElementById("namuru3_audio");
     var audio_rand = 0;
     $(document).on('click', 'button.star-icon', function() {
         audio_rand = Math.floor( Math.random() * 10000 );
+        console.log("南無！");
         if (audio_rand < 334) {
-            console.log("レア南無！");
             audio_d2.currentTime = 0;
             audio_d2.play();
+        } else if (audio_rand < 5167) { //適当
+            audio_d3.currentTime = 0;
+            audio_d3.play();
         } else {
-            console.log("南無！");
             audio_d.currentTime = 0;
             audio_d.play();
         }
