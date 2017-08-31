5a2e9db334846b7f3cbcfe46fb82fb8df92bbf37
yuzu_1203 <git-yuzu@svk.jp>
2017-08-31 11:07:25 +0900

    v1.3.0 アップデート

--- Namuru-button.user.js
+++ Namuru-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Namuru-button
 // @namespace    https://github.com/yuzulabo
-// @version      1.2.1
+// @version      1.3.0
 // @description  南無りまくれぇ！
 // @author       neziri_wasabi (yuzu_1203)
 // @match        https://knzkoniisan.m.to/*
@@ -17,7 +17,8 @@
 フリー素材を使用しています。http://nico.ms/nc42872
 
  レア音声：神崎おにいさんについて
- 可視光線様のKNZK_ZONEの一部を使用しています。: http://nico.ms/sm31146004
+ 1: 可視光線様のKNZK_ZONEの一部を使用しています。: http://nico.ms/sm31146004
+ 2: 「溺れ死ぬボーちゃんの深海少女。を歌ってみた」の一部を使用しています。：http://nico.ms/sm31734029
 
  これらの音声はスクリプト作者の個人サーバにアップロードされていますが、問題があればお問い合わせください。
  連絡先: https://knzkoniisan.m.to/@y
@@ -28,46 +29,59 @@
     node.type = "text/css";
     node.appendChild(document.createTextNode(css));
 
-    var audio_elem = document.createElement("audio");
-    audio_elem.src = "https://yuzu.tk/files/yuzu_1203/gooooon.wav";
-    audio_elem.setAttribute('controls', '0');
-    audio_elem.setAttribute('id', 'namuru_audio');
-    audio_elem.setAttribute('preload', 'auto');
-
-    var audio2_elem = document.createElement("audio");
-    audio2_elem.src = "https://yuzu.tk/files/yuzu_1203/knzk.wav";
-    audio2_elem.setAttribute('controls', '0');
-    audio2_elem.setAttribute('id', 'namuru2_audio');
-    audio2_elem.setAttribute('preload', 'auto');
-
-    var audio3_elem = document.createElement("audio");
-    audio3_elem.src = "https://yuzu.tk/files/yuzu_1203/nc42872.wav";
-    audio3_elem.setAttribute('controls', '0');
-    audio3_elem.setAttribute('id', 'namuru3_audio');
-    audio3_elem.setAttribute('preload', 'auto');
-
     var heads = document.getElementsByTagName("head");
     heads[0].appendChild(node);
-    heads[0].appendChild(audio_elem);
-    heads[0].appendChild(audio2_elem);
-    heads[0].appendChild(audio3_elem);
 
-    var audio_d = document.getElementById("namuru_audio");
-    var audio_d2 = document.getElementById("namuru2_audio");
-    var audio_d3 = document.getElementById("namuru3_audio");
+    var audio_element  = Array();
+    var audio_src  = Array();
+    var audio_d = Array();
+    var rand_sp = Array();
+    var rand_def = Array();
+
+    rand_def[0] = 0;
+    audio_src[0] = "https://yuzu.tk/files/yuzu_1203/gooooon.wav";
+
+    rand_sp[0] = 1;
+    audio_src[1] = "https://yuzu.tk/files/yuzu_1203/knzk.wav";
+
+    rand_def[1] = 2;
+    audio_src[2] = "https://yuzu.tk/files/yuzu_1203/nc42872.wav";
+
+    rand_sp[1] = 3;
+    audio_src[3] = "https://yuzu.tk/files/yuzu_1203/knzk2.wav";
+
+    var i = 0;
+    while (audio_src[i]) {
+        audio_element[i] = document.createElement("audio");
+        audio_element[i].src = audio_src[i];
+        audio_element[i].setAttribute('controls', '0');
+        audio_element[i].setAttribute('id', 'namuru'+i+'_audio');
+        audio_element[i].setAttribute('preload', 'auto');
+
+        heads[0].appendChild(audio_element[i]);
+        audio_d[i] = document.getElementById('namuru'+i+'_audio');
+        i++;
+    }
+
+    var rand_def_mode = 0;
+    var rand_sp_mode = 0;
     var audio_rand = 0;
+    var audio_mode = 9999;
     $(document).on('click', 'button.star-icon', function() {
+        audio_mode = 9999;
         audio_rand = Math.floor( Math.random() * 10000 );
-        console.log("南無！");
+        rand_def_mode = Math.floor(Math.random()*rand_def.length);
+        rand_sp_mode = Math.floor(Math.random()*rand_sp.length);
+
         if (audio_rand < 334) {
-            audio_d2.currentTime = 0;
-            audio_d2.play();
-        } else if (audio_rand < 5167) { //適当
-            audio_d3.currentTime = 0;
-            audio_d3.play();
+            audio_mode = rand_sp[rand_sp_mode];
         } else {
-            audio_d.currentTime = 0;
-            audio_d.play();
+            audio_mode = rand_def[rand_def_mode];
         }
+
+        audio_d[audio_mode].currentTime = 0;
+        audio_d[audio_mode].play();
+
+        console.log("南無！");
     });
 })();
