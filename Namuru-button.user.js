a48266a3b7eafc3a11d6815ecad9743770a47da5
yuzulabo <git-yuzu@svk.jp>
2017-09-02 17:17:16 +0900

    Knzkru-button init

--- Namuru-button.user.js
+++ Namuru-button.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Namuru-button
+// @name         Knzkru-button
 // @namespace    https://github.com/yuzulabo
-// @version      1.4.0
-// @description  南無りまくれぇ！
+// @version      1.0.0
+// @description  南無りまくれぇ！(神崎おにいさんver)
 // @author       neziri_wasabi (yuzu_1203)
 // @match        https://knzkoniisan.m.to/*
 // @license       MIT License
@@ -10,27 +10,17 @@
 // ==/UserScript==
 
 /*
- 「ゴーン」効果音について
-フリー素材を使用しています。魔王魂: http://maoudamashii.jokersounds.com/
 
-「チーン」効果音について
-フリー素材を使用しています。http://nico.ms/nc42872
-
- 木魚　効果音について
- フリー素材を使用しています。http://nico.ms/nc92438
-
- レア音声：神崎おにいさんについて
+ 神崎おにいさんボイスについて
  1: 可視光線様のKNZK_ZONEの一部を使用しています。: http://nico.ms/sm31146004
  2: 「溺れ死ぬボーちゃんの深海少女。を歌ってみた」の一部を使用しています。：http://nico.ms/sm31734029
 
  これらの音声はスクリプト作者の個人サーバにアップロードされていますが、問題があればお問い合わせください。
+
  連絡先: https://knzkoniisan.m.to/@y
 */
 (function() {
     var css = ".star-icon {filter: grayscale(100%);}.fa-star:before{content: \"\\1F64F\";white-space: nowrap;} .star-icon.active {filter: none;}";
-
-    // 旧版「ﾅﾑ」ボタンを有効化するには次の行のコメントアウトを削除
-    // css = ".fa-star:before {content: \"ﾅﾑ\";white-space: nowrap;}";
 
     var node = document.createElement("style");
     node.type = "text/css";
@@ -42,23 +32,10 @@
     var audio_element  = Array();
     var audio_src  = Array();
     var audio_d = Array();
-    var rand_sp = Array();
-    var rand_def = Array();
 
-    rand_def[0] = 0;
-    audio_src[0] = "https://yuzu.tk/files/yuzu_1203/gooooon.wav";
+    audio_src[0] = "https://yuzu.tk/files/yuzu_1203/knzk.wav";
 
-    rand_sp[0] = 1;
-    audio_src[1] = "https://yuzu.tk/files/yuzu_1203/knzk.wav";
-
-    rand_def[1] = 2;
-    audio_src[2] = "https://yuzu.tk/files/yuzu_1203/nc42872.wav";
-
-    rand_sp[1] = 3;
-    audio_src[3] = "https://yuzu.tk/files/yuzu_1203/knzk2.wav";
-
-    rand_def[2] = 4;
-    audio_src[4] = "https://yuzu.tk/files/yuzu_1203/nc92438.wav";
+    audio_src[1] = "https://yuzu.tk/files/yuzu_1203/knzk2.wav";
 
     var i = 0;
     while (audio_src[i]) {
@@ -73,21 +50,10 @@
         i++;
     }
 
-    var rand_def_mode = 0;
-    var rand_sp_mode = 0;
-    var audio_rand = 0;
     var audio_mode = 9999;
     $(document).on('click', 'button.star-icon', function(event) {
         audio_mode = 9999;
-        audio_rand = Math.floor( Math.random() * 10000 );
-        rand_def_mode = Math.floor(Math.random()*rand_def.length);
-        rand_sp_mode = Math.floor(Math.random()*rand_sp.length);
-
-        if (audio_rand < 334) {
-            audio_mode = rand_sp[rand_sp_mode];
-        } else {
-            audio_mode = rand_def[rand_def_mode];
-        }
+        audio_mode = Math.floor(Math.random()*audio_src.length);
 
         audio_d[audio_mode].currentTime = 0;
         audio_d[audio_mode].play();
